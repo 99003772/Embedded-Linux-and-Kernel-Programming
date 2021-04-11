e479a899b223ad17bda995c7846b850ffe8dde6d
99003772 <78854464+99003772@users.noreply.github.com>
2021-04-11 13:45:07 +0530

    Update threads.c

--- Hands on/Kthread/threads.c
+++ Hands on/Kthread/threads.c
@@ -1,35 +1,14 @@
 #include <linux/fs.h>
- 
-
 #include <linux/init.h>
- 
-
 #include <linux/module.h>
- 
-
 #include <linux/kernel.h>
- 
-
 #include <linux/cdev.h>
- 
-
 #include <linux/device.h>
- 
-
 #include <linux/slab.h>
-
-
 #include <linux/uaccess.h>
-
-
 #include <linux/delay.h>
-
-
 #include <linux/kthread.h>
- 
-
 // Global
- 
 static struct task_struct *task1;
 static struct task_struct *task2;
 static int thread_one(void *pargs)
@@ -43,11 +22,6 @@ static int thread_one(void *pargs)
     do_exit(0);
     return 0;
 }
-
- 
-
- 
-
 static int thread_two(void *pargs)
 {
     int k;
@@ -59,72 +33,24 @@ static int thread_two(void *pargs)
     do_exit(0);
     return 0;
 }
-
- 
-
- 
-
- 
-
 static int __init tdemo_init(void)
-
- 
-
 {
- 
-
-    task1=kthread_run(thread_one, NULL, "thread_A");
-    
+    task1=kthread_run(thread_one, NULL, "thread_A");   
     task2=kthread_run(thread_two, NULL, "thread_B");
-    
     printk("kernel threads..welcome\n");
-
     return 0;
-
-
 }
-
-
-
- 
-
 static void __exit tdemo_exit(void)
-
- 
-
 {    
- 
-
     if(task1)
         kthread_stop(task1);
     if(task2)
         kthread_stop(task2); 
 
     printk("kernel threads..Bye\n");
-
- 
 }
-
- 
-
- 
-
- 
-
 module_init(tdemo_init);
-
- 
-
 module_exit(tdemo_exit);
-
- 
-
 MODULE_LICENSE("GPL");
-
- 
-
 MODULE_AUTHOR("SAYANI");
-
- 
-
 MODULE_DESCRIPTION("Kernel threads");
