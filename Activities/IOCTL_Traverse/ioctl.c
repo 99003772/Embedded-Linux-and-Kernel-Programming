3a02de5f5332e3e9a10db4079db9e28ca0dcf7df
99003772 <78854464+99003772@users.noreply.github.com>
2021-04-11 23:34:30 +0530

    Update ioctl.c

--- Activities/IOCTL_Traverse/ioctl.c
+++ Activities/IOCTL_Traverse/ioctl.c
@@ -21,9 +21,7 @@
 
 void printdata(void);
 struct device *pdev; //global
-
 struct class *pclass; //global
-
 struct cdev cdev;
 LIST_HEAD(mylist);
 struct sample {
@@ -32,30 +30,18 @@ struct sample {
   int z;
   struct list_head entry;
 };
-
-
 int ndevices=1;
-
 int i=0;
-
 unsigned char *pbuffer;
-
 int rd_offset=0;
-
 int wr_offset=0;
-
 int buflen=0;
-
 char tbuf;
 int n;
 char ret;
-
 int rcount;
-
 int wcount;
-
 struct kfifo myfifo;
-
 static long pseudo_ioctl(struct file *file,unsigned int cmd, unsigned long arg)
 {
 char buf[50];
@@ -94,69 +80,38 @@ void printdata(void) {
 
 struct file_operations fops =
 {
-
   .unlocked_ioctl = pseudo_ioctl
-
 };
-
 dev_t pdevid;
-
 static int __init psuedo_init(void)
-
 {
-
   int ret, i=0;
-
   pclass = class_create(THIS_MODULE, "pseudo_class");
-
   ret=alloc_chrdev_region(&pdevid, 0, ndevices, "pseudo_file_driver_sample");
-
   cdev_init(&cdev, &fops);
-
   pbuffer = kmalloc(MAX_SIZE, GFP_KERNEL);
-
   kfifo_init(&myfifo, pbuffer,8);
-
   kobject_set_name(&cdev.kobj,"pdevice%d",i);
-
   ret = cdev_add(&cdev, pdevid, 1);
-
   pdev = device_create(pclass, NULL, pdevid, NULL, "psample%d",i);
-
   if(ret) {
-
   printk("Pseudo: Failed to register driver\n");
-
   return -EINVAL;
-
   }
-
   printk("Successfully registered,major=%d,minor=%d\n",
-
   MAJOR(pdevid), MINOR(pdevid));
-
   printk("Pseudo Driver Sample..welcome\n");
-
   return 0;
-
 }
 
 static void __exit psuedo_exit(void) 
-
 {
-
   device_destroy(pclass, pdevid);
-
   cdev_del(&cdev);
-
   unregister_chrdev_region(pdevid, ndevices);
-
   kfifo_free(&myfifo);
-
   class_destroy(pclass);
-
   kfree(pbuffer);
-  
     struct list_head *pcur,*pbak;
   struct sample *ps;
   list_for_each_safe(pcur,pbak, &mylist) {
@@ -164,9 +119,7 @@ static void __exit psuedo_exit(void)
     kfree(ps);
   }
   printk("Bye,Leaving the world\n");
-
   printk("Pseudo Driver Sample..Bye\n");
-
 }
 
 module_init(psuedo_init);
@@ -175,6 +128,6 @@ module_exit(psuedo_exit);
 
 MODULE_LICENSE("GPL");
 
-MODULE_AUTHOR("Nitin N Shetty");
+MODULE_AUTHOR("Sayani Basak");
 
 MODULE_DESCRIPTION("A PSEUDO MODULE");
